7d8e517a2a23757bf3471aa76e46176bc8f101cd
KLM-Nandhu <167982424+KLM-Nandhu@users.noreply.github.com>
2024-09-25 13:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ st.markdown("""
         font-weight: bold;
         border-radius: 8px;
         padding: 0.5rem 1rem;
-        aligin
+        position: relative;
         width: 50%;
     }
     .stButton>button:hover {
